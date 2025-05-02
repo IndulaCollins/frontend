92083e81ecacaab7bcdaa18ceedefa7dd1387a9e
IndulaCollins <158047417+IndulaCollins@users.noreply.github.com>
2025-05-02 12:06:18 +0300

    Update Headsection.jsx

--- components/Headsection.jsx
+++ components/Headsection.jsx
@@ -16,23 +16,23 @@ const Headsection = () => {
 
         <section className='grid md:grid-cols-5 gap-8 py-12 px-12 bg-gray-100 md:px-20'>
           <div className="shadow-lg p-6 rounded bg-white">
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='w-full'></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='size-auto'></iframe>
           </div>
 
           <div className="shadow-lg p-6 rounded bg-white">
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='size-fit'></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" ></iframe>
           </div>
 
           <div className="shadow-lg p-6 rounded bg-white">
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='size-fit'></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" ></iframe>
           </div>
 
           <div className="shadow-lg p-6 rounded bg-white">
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='size-fit'></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" ></iframe>
           </div>
 
           <div className="shadow-lg p-6 rounded bg-white">
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" className='size-fit'></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7297807241011699712?collapsed=1" frameborder="0" allowfullscreen="" title="Embedded post" ></iframe>
           </div>
         </section>
 
